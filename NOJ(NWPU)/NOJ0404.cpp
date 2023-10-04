518b6119e6a73ad08786f8aed59ad5d001486033
asandstar <89233112+asandstar@users.noreply.github.com>
2023-10-04 15:43:19 +0800

    Update NOJ0404.cpp

--- NOJ(NWPU)/NOJ0404.cpp
+++ NOJ(NWPU)/NOJ0404.cpp
@@ -1,3 +1,10 @@
+/*
+题目内容：已知ack函数对于m≥0和n≥0有定义：ack(0,n)=n+1、ack(m,0)=ack(m-1,1)、ack(m,n)=ack(m-1,ack(m,n-1))。输入m和n，求解ack函数。
+输入格式：输入m和n，均为整型，用空格隔开。
+输出格式：输出为整型
+输入样例：3 2
+输出样例：29
+*/
 #include <iostream>
 using namespace std;
 int ack(int m, int n)
