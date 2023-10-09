c06b1239f68727944d7c85f17230b63612a48df6
asandstar <89233112+asandstar@users.noreply.github.com>
2023-10-09 17:03:51 +0800

    Update NOJ0703.cpp

--- NOJ(NWPU)/NOJ0703.cpp
+++ NOJ(NWPU)/NOJ0703.cpp
@@ -1,3 +1,11 @@
+/*
+题目内容：编写函数void strencode(char *s)；函数的功能是将字符串中的大写字母加3，
+小写字母减3。在主函数中输入字符串，调用函数后输出结果字符串。
+输入格式：输入一行字符串。
+输出格式：输出编码操作后的字符串。
+输入样例：ABCDEF
+输出样例：DEFGHI
+*/
 #include <iostream>
 #include <cstring>
 using namespace std;
