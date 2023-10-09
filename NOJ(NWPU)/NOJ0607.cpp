086bee433b8ddb7e18054a4fc79bbc2d61e576a2
asandstar <89233112+asandstar@users.noreply.github.com>
2023-10-09 16:50:11 +0800

    Update NOJ0607.cpp

--- NOJ(NWPU)/NOJ0607.cpp
+++ NOJ(NWPU)/NOJ0607.cpp
@@ -1,3 +1,13 @@
+/*
+题目内容：请编写程序，对键盘输入的字符串进行逆序，逆序后的字符串仍然保留在原来字符数组中，最后输出。
+输入格式：输入字符串时，输入以等号（=）结束（注意不是回车）
+输出格式：输出为字符串。
+输入样例：
+hello world=
+dlrow olleh
+输出样例：
+dlrow olleh
+*/
 #include <iostream>
 #include <cstring>
 using namespace std;
