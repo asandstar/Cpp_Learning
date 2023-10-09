6e6c4847a229709f62e0899e43329b930be2d646
asandstar <89233112+asandstar@users.noreply.github.com>
2023-10-09 16:53:51 +0800

    Update NOJ0611.cpp

--- NOJ(NWPU)/NOJ0611.cpp
+++ NOJ(NWPU)/NOJ0611.cpp
@@ -1,3 +1,11 @@
+/*
+题目内容：编写程序以字符串为单位，以空格或标点符号（字符串中仅含英文逗号','或小数点'.'作为标点符号）作为分隔符，
+对字符串中所有单词进行倒排，然后把已处理的字符串（应不含标点符号）打印出来。
+输入格式：输入一个字符串（包含大小写字母、空格、逗号或小数点）
+输出格式：输出处理后的字符串。
+输入样例：I am a student. I like study.
+输出样例：study like I student a am I
+*/
 #include <iostream>
 #include <string>
 using namespace std;
