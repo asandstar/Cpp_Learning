b7bed083615f08ab43deb781eec3afc4cfce8538
asandstar <89233112+asandstar@users.noreply.github.com>
2023-10-09 16:46:28 +0800

    Update NOJ0605.cpp

--- NOJ(NWPU)/NOJ0605.cpp
+++ NOJ(NWPU)/NOJ0605.cpp
@@ -1,3 +1,10 @@
+/*
+题目内容：在主函数中输入10个等长的字符串（每个字符串最多10个字符），用另一个函数对它们进行由小到大排序，然后在主函数中输出这10个已排好序的字符串。
+输入格式：输入10个等长的字符串，用空格分隔。
+输出格式：输出排序后的10个字符串，用空格分隔。
+输入样例：she its can ibm bbc NBA nhk BOY jxf eat
+输出样例：BOY NBA bbc can eat ibm its jxf nhk she
+*/
 #include <iostream>
 #include <algorithm>
 #include <string>
